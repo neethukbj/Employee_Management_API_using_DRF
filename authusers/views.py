a9e617dd7cb6af0946fd8569f5f1504511dd314b
Neethu Babu <neethubabu212@gmail.com>
2024-10-31 04:05:54 +0530

    Add JWT authentication with user registration and login endpoints

---
+++ authusers/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from .serializers import *
+from django.contrib.auth.models import User
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework import status
+
+class RegisterView(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+
+class LoginView(generics.GenericAPIView):
+	
+	serializer_class=LoginSerializer
+	
+	def post(self,request):
+                try:
+                    data = request.data
+                    serializer = self.get_serializer(data=data)
+                    serializer.is_valid(raise_exception=True)
+                    response = serializer.get_jwt_token(serializer.validated_data)
+                    return Response(response, status=status.HTTP_200_OK)
+        
+                except serializers.ValidationError as e:
+                    return Response({"data": e.detail, "message": "Invalid credentials..!"}, status=status.HTTP_400_BAD_REQUEST)
+
+                except Exception as e:
+                    return Response({"data": {}, "message": "Something went wrong..!"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)    
+                
+
+                
--- employee_management/urls.py
+++ employee_management/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("api/auth/",include("authusers.urls")),
+    #path("swagger/",schema_view.with_ui("swagger",cache_timeout=0),name="schema_swagger_ui"),
+    #path("redoc/",schema_view.with_ui("redoc",cache_timeout=0),name="schema_redoc"),
 ]
