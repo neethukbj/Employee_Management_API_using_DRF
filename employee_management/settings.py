7177bf5550bd265336368645eb61528eb4d93d06
Neethukbj <neethubabu212@gmail.com>
2024-11-01 02:05:01 +0530

    Integrate Swagger for API documentation

--- employee_management/settings.py
+++ employee_management/settings.py
@@ -161,16 +161,16 @@ REST_FRAMEWORK = {
 }
 
 
-# SWAGGER_SETTINGS = {
-#     'SECURITY_DEFINITIONS': {
-#         'Bearer': {
-#             'type': 'apiKey',
-#             'name': 'Authorization',
-#             'in': 'header'
-#         }
-#     },
-#     'USE_SESSION_AUTH': False,
-#     'DEFAULT_PERMISSION_CLASSES': [
-#         'rest_framework.permissions.IsAuthenticated',
-#     ],
-# }
+SWAGGER_SETTINGS = {
+    'SECURITY_DEFINITIONS': {
+        'Bearer': {
+            'type': 'apiKey',
+            'name': 'Authorization',
+            'in': 'header'
+        }
+    },
+    'USE_SESSION_AUTH': False,
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
