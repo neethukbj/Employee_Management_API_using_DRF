1a1216a2e8c6c768b326bb6cb30f3cf00ee8310f
Neethukbj <neethubabu212@gmail.com>
2024-11-01 23:57:34 +0530

    Fix CORS for live deployment

--- employee_management/settings.py
+++ employee_management/settings.py
@@ -43,10 +43,12 @@ INSTALLED_APPS = [
     'drf_yasg',
     'authusers',
     'employees',
+    'corsheaders',
 
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -55,6 +57,11 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+# CORS_ALLOWED_ORIGINS = [
+#     "https://employee-management-15jq.onrender.com",  
+# ]
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'employee_management.urls'
 
