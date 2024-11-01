91c206b5224d6dd88373b7f9176288b3578f9abe
Neethukbj <neethubabu212@gmail.com>
2024-11-01 22:43:17 +0530

    Configure Django project for production

--- employee_management/settings.py
+++ employee_management/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0d$()(xbdx%i$vsd3jvxby=drtt%okl$9eh@p^t30%t@^#$oq-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
